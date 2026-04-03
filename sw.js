ee3904891e83330d0f5e639eb4c59a6d5e25988f
Sky-NS <64040977+Sky-NS@users.noreply.github.com>
2026-04-03 17:13:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,61 @@
+
+const CACHE_NAME = 'jtc-cache-v1';
+const PRECACHE = [
+  './',
+  './index.html',
+  './planner.html',
+  './osaka.html',
+  './fuji.html',
+  './tokyo.html',
+  './shanghai.html',
+  './budget.html',
+  './visa.html',
+  './glossary.html',
+  './contacts.html',
+  './toilet-map.html',
+  './manifest.json',
+  './CSS/style.css',
+  './js/data.js',
+  './js/currency.js',
+  './js/app.js',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
